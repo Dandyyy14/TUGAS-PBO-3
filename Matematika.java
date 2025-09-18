5861fb6df9d05678d6d42bb2510b7cd471d2979b
Dandyyy14 <muhammaddandy1899@gmail.com>
2025-09-18 18:42:43 +0700

    Create Matematika.java

---
+++ Matematika.java
@@ -0,0 +1,14 @@
+public class Matematika {
+    int pertambahan(int a, int b) {
+        return a + b;
+    }
+    int pengurangan(int a, int b) {
+        return a - b;
+    }
+    int perkalian(int a, int b) {
+        return a * b;
+    }
+    double pembagian(double a, double b) {
+        return a / b;
+    }
+}
